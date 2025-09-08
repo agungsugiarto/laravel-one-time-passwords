bf72403d24b83148c2f2b71f622ef0c068c4c72a
Agung Sugiarto <sugiartoagung92@gmail.com>
2025-09-08 09:47:19 +0700

    Support PHP ^8.1 and Laravel ^10

--- tests/TestSupport/TestCase.php
+++ tests/TestSupport/TestCase.php
@@ -18,6 +18,9 @@ class TestCase extends Orchestra
 
         config()->set('cache.default', 'array');
 
+        // Disable rate limiting for tests
+        config()->set('one-time-passwords.rate_limit_attempts.max_attempts_per_user', 999);
+
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'Spatie\\OneTimePasswords\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
